e5cd0edb909f1b8f6501db5b392f16b8398bd8f4
Munashe02 <gwazemunashe02@gmail.com>
2026-09-17 16:25:17 +0200

    Add cPanel production server

---
+++ server.cjs
@@ -0,0 +1,15 @@
+const http = require('http')
+const next = require('next')
+
+const hostname = process.env.HOSTNAME || '0.0.0.0'
+const port = Number.parseInt(process.env.PORT || '3000', 10)
+const app = next({ dev: false, hostname, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  http
+    .createServer((request, response) => handle(request, response))
+    .listen(port, hostname, () => {
+      console.log(`D7 TechNode is running on http://${hostname}:${port}`)
+    })
+})

3f289d59a6818b4b405eb7537f483b9d5c78122c
Munashe02 <gwazemunashe02@gmail.com>
2026-09-17 18:08:46 +0200

    Add standard cPanel Passenger entry point

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
 const http = require('http')
 const next = require('next')
 
-const hostname = process.env.HOSTNAME || '0.0.0.0'
+const hostname = process.env.APP_HOST || '0.0.0.0'
 const port = Number.parseInt(process.env.PORT || '3000', 10)
 const app = next({ dev: false, hostname, port })
 const handle = app.getRequestHandler()
